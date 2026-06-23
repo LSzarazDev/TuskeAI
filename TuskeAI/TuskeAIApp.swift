cf3677f1a6ecf561a963ba4c08c8ef18667ae680
LSzarazDev <a_te_email_cmed@example.com>
2026-06-23 21:38:08 +0200

    Initial Commit

---
+++ TuskeAI/TuskeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuskeAIApp.swift
+//  TuskeAI
+//
+//  Created by Száraz Lóránt on 2026. 06. 23..
+//
+
+import SwiftUI
+
+@main
+struct TuskeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
